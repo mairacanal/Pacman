c8ae29cd4279cd2d1ff753868e9ffe45a0977961
Maíra Canal <maira.canal@usp.br>
2021-11-02 10:02:35 -0300

    GameConstants: create class with all game constants

---
+++ src/pacman/gameElements/GameConstants.java
@@ -0,0 +1,25 @@
+package pacman.gameElements;
+
+public final class GameConstants {
+
+    static final int BOARD_VERTICAL = 31;
+    static final int BOARD_HORIZONTAL = 28;
+
+    static final int PATH = 0;
+    static final int BLOCKED = 1;
+
+    static final int PACMAN = 10;
+    static final int BLINKY = 11;
+    static final int PINKY = 12;
+    static final int INKY = 13;
+    static final int CLYDE = 14;
+
+    static final int PACDOT = 20;
+    static final int PILL = 21;
+    static final int FRUIT = 22;
+
+    static final int HOME = 30;
+    static final int GATE = 31;
+    static final int WALL = 32;
+    
+}
