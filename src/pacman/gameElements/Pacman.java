40eff8557df2b77e9d24f94095dc39e6f03170ca
Maíra Canal <maira.canal@usp.br>
2021-11-09 19:25:51 -0300

    Pacman: Refactor Pacman movimentation

--- src/pacman/gameElements/Pacman.java
+++ src/pacman/gameElements/Pacman.java
@@ -1,15 +1,14 @@
 package pacman.gameElements;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Pacman extends Entity {
-    
-    private String direction;
     
     public Pacman(Node node) {
         
         // definir identificaçao e velocidade
-        super(node, GameConstants.PACMAN, 0);
+        super(node, GameConstants.PACMAN, 0, GameConstants.LEFT);
         
     }
     
@@ -20,15 +19,23 @@ public class Pacman extends Entity {
     // TODO: se mover por meio de entrada de teclado
     public void move() {
 
+        Random rand = new Random();
         Node previousNode = node;
         ArrayList<Node> nodes = node.getNodes();
+        ArrayList<Integer> id = new ArrayList<>();
+        int newDirection;
 
-        while (!node.isCanWalk()) {
+        id.add(GameConstants.BLOCKED);
+        id.add(GameConstants.GATE);
 
-            node = nodes.get((int) (Math.random() * 3) + 1);
+        do {
 
-        }
+            newDirection = rand.nextInt(4);
+            node = nodes.get(newDirection);
 
+        } while (!node.notForbiddenId(id) || newDirection == GameConstants.oppositeDirection(direction));
+
+        direction = newDirection;
         previousNode.removeEntity(this);
         node.addEntity(this);
         
