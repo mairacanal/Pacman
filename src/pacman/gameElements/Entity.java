9afb04e8ee3491c6c878eed10d7fbaad4a72a0aa
Maíra Canal <maira.canal@usp.br>
2021-11-09 19:27:43 -0300

    Entity: Add getter to the Entity

--- src/pacman/gameElements/Entity.java
+++ src/pacman/gameElements/Entity.java
@@ -3,14 +3,16 @@ package pacman.gameElements;
 public abstract class Entity {
     
     protected Node node;
-    protected int type;
+    protected int id;
     protected int speed;
+    protected int direction;
     
-    Entity(Node node, int type, int speed) {
+    Entity(Node node, int id, int speed, int direction) {
         
         this.node = node;
-        this.type = type;
+        this.id = id;
         this.speed = speed;
+        this.direction = direction;
 
         this.node.addEntity(this);
         
@@ -19,5 +21,17 @@ public abstract class Entity {
     abstract public void move();
     abstract public void born();
     abstract public void die();
+
+    public int getId() {
+
+        return id;
+
+    }
+
+    public Node getNode() {
+
+        return node;
+
+    }
     
 }
