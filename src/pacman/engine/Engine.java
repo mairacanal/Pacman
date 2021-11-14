fb866f75e8cec39387a073914947cc0c2280bb61
Maíra Canal <maira.canal@usp.br>
2021-11-14 19:05:05 -0300

    Engine: Implement main loops

--- src/pacman/engine/Engine.java
+++ src/pacman/engine/Engine.java
@@ -1,22 +1,18 @@
 package pacman.engine;
 
 import pacman.gameElements.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.ArrayList;
+
+import java.util.concurrent.TimeUnit;
 
 public class Engine {
 
     private Map map;
-    private GameLoop gameLoop;
     private GameRules gameRules;
     private ArrayList<Entity> entities;
 
-    private final int framerate = 30;
-    private final int BOARD_VERTICAL = GameConstants.BOARD_VERTICAL;
-    private final int BOARD_HORIZONTAL = GameConstants.BOARD_HORIZONTAL;
+    private final int framerate = 1;
 
     /**
      * 
@@ -26,88 +22,10 @@ public class Engine {
 
        this.map = map;
        this.entities = new ArrayList<>();
-       this.gameLoop = new GameLoop(entities);
        this.gameRules = new GameRules(map);
 
     }
 
-    /**
-     * 
-     * @return
-     */
-    private int[][] readMapFile() {
-
-        int[][] matrix = new int[BOARD_VERTICAL][BOARD_HORIZONTAL];
-
-        try {
-
-            File mapFile = new File("src/pacman/resources/map.txt");
-            Scanner scan = new Scanner(mapFile);
-
-            for (int i = 0; i < BOARD_VERTICAL; i++)
-                for (int j = 0; j < BOARD_HORIZONTAL; j++)
-                    matrix[i][j] = scan.nextInt();
-            
-        } catch (FileNotFoundException e) {
-
-            System.out.println("An error occurred: " + e.getMessage());
-        }
-
-        return matrix;
-        
-    }
-
-    /**
-     * 
-     * @param matrix
-     */
-    private void buildMap(int[][] matrix) {
-
-        Entity entity;
-        Node[][] nodes;
-
-        nodes = new Node[BOARD_VERTICAL][BOARD_HORIZONTAL];
-
-        for (int i = 0; i < BOARD_VERTICAL; i++) {
-            for (int j = 0; j < BOARD_HORIZONTAL; j++) {
-                
-                int id = matrix[i][j];
-
-                if (id >= GameConstants.PACDOT && id <= GameConstants.FRUIT)
-                    nodes[i][j] =  new Node(id, new Consumable(id));
-                else 
-                    nodes[i][j] =  new Node(id);
-
-                switch (id) {
-                    case (GameConstants.PACMAN):
-                        entity = new Pacman(nodes[i][j]);
-                        break;
-                    case (GameConstants.BLINKY):
-                        entity = new Blinky(nodes[i][j]);
-                        break;
-                    case (GameConstants.PINKY):
-                        entity = new Pinky(nodes[i][j]);
-                        break;
-                    case (GameConstants.INKY):
-                        entity = new Inky(nodes[i][j]);
-                        break;
-                    case (GameConstants.CLYDE):
-                        entity = new Clyde(nodes[i][j]);
-                        break;
-                    default:
-                        entity = null;
-                        break;
-                }
-
-                if (entity != null)
-                    entities.add(entity);
-            
-            }
-        }
-
-        map.build(nodes);
-
-    }
 
     /**
      * 
@@ -155,27 +73,15 @@ public class Engine {
     /**
      * 
      */
-    private void resetMap() {
-
-        int[][] matrix = readMapFile();
-        
-        entities.clear();
-
-        buildMap(matrix);
-
-    }
-
-    /**
-     * 
-     */
     public void init() {
 
-        resetMap();
+        entities = map.build();
         
         calculateDistance(entities.get(entities.size() - 1).getNode(), "pacman");
         calculateDistance(map.getNode(11, 13), "ghostSidewalk");
 
-        gameLoop.init();
+        for (Entity entity : entities)
+            entity.born();
 
     }
 
@@ -184,8 +90,32 @@ public class Engine {
      */
     public void running() {
 
-        gameLoop.loop();
+        try {
+
+            TimeUnit.MILLISECONDS.sleep(1000 / framerate);
+
+        } catch (InterruptedException e) {
+
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+        for (Entity entity : entities)
+            entity.move();
         gameRules.runAllRules();
+
+        if (GameStatus.isResetGame()) {
+
+            for (Entity entity : entities)
+                entity.born();
+            GameStatus.setResetGame(false);
+
+        } else if (GameStatus.isGameOver()) {
+
+            GameStatus.setGameOver(false);
+            exit();
+
+        }
 
         calculateDistance(entities.get(entities.size() - 1).getNode(), "pacman");
 
@@ -195,6 +125,10 @@ public class Engine {
      * 
      */
     public void exit() {
+
+        System.out.println("GAME OVER");
+        System.out.println("Sua pontuação: " + GameStatus.getPoints());
+        System.exit(0);
 
     }
 
--- src/pacman/gameElements/Map.java
+++ src/pacman/gameElements/Map.java
@@ -1,11 +1,17 @@
 package pacman.gameElements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  * 
  */
 public class Map {
     
     private Node[][] nodes;
+    private int[][] matrix;
 
     private final int BOARD_VERTICAL = GameConstants.BOARD_VERTICAL;
     private final int BOARD_HORIZONTAL = GameConstants.BOARD_HORIZONTAL;
@@ -16,9 +22,37 @@ public class Map {
     public Map(){
         
         this.nodes = new Node[BOARD_VERTICAL][BOARD_HORIZONTAL];
-    
+        this.matrix = readMapFile();
+
     }
     
+    
+    /**
+     * 
+     * @return
+     */
+    private int[][] readMapFile() {
+
+        int[][] matrix = new int[BOARD_VERTICAL][BOARD_HORIZONTAL];
+
+        try {
+
+            File mapFile = new File("src/pacman/resources/map.txt");
+            Scanner scan = new Scanner(mapFile);
+
+            for (int i = 0; i < BOARD_VERTICAL; i++)
+                for (int j = 0; j < BOARD_HORIZONTAL; j++)
+                    matrix[i][j] = scan.nextInt();
+            
+        } catch (FileNotFoundException e) {
+
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+
+        return matrix;
+        
+    }
+
     /**
      * 
      */
@@ -35,6 +69,52 @@ public class Map {
             }
         }
         
+    }
+
+    /**
+     * 
+     * @param matrix
+     */
+    public ArrayList<Entity> build() {
+
+        ArrayList<Entity> entities = new ArrayList<>();
+
+        for (int i = 0; i < BOARD_VERTICAL; i++) {
+            for (int j = 0; j < BOARD_HORIZONTAL; j++) {
+                
+                int id = matrix[i][j];
+
+                if (id >= GameConstants.PACDOT && id <= GameConstants.FRUIT)
+                    nodes[i][j] =  new Node(id, new Consumable(id));
+                else 
+                    nodes[i][j] =  new Node(id);
+
+                switch (id) {
+                    case (GameConstants.PACMAN):
+                        entities.add(new Pacman(nodes[i][j]));
+                        break;
+                    case (GameConstants.BLINKY):
+                        entities.add(new Blinky(nodes[i][j]));
+                        break;
+                    case (GameConstants.PINKY):
+                        entities.add(new Pinky(nodes[i][j]));
+                        break;
+                    case (GameConstants.INKY):
+                        entities.add(new Inky(nodes[i][j]));
+                        break;
+                    case (GameConstants.CLYDE):
+                        entities.add(new Clyde(nodes[i][j]));
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        linkAll();
+        return entities;
+
     }
     
     /**
@@ -57,17 +137,6 @@ public class Map {
         
         return nodes;
     
-    }
-    
-    /**
-     * 
-     * @param nodes
-     */
-    public void build(Node[][] nodes){   
-        
-        this.nodes = nodes;
-        linkAll();
-        
-    }  
-    
+    }    
+
 }
