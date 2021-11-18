f1834a27dcd448ba58791a599fc05ba58c38d4b8
Maíra <maira.canal@usp.br>
2021-11-18 16:00:14 -0300

    Clyde: Add Javadoc

--- src/pacman/gameElements/Clyde.java
+++ src/pacman/gameElements/Clyde.java
@@ -1,23 +1,25 @@
 package pacman.gameElements;
 
 /**
- * 
+ * Classe que representa um elemento do jogo, o fantasma Clyde 
  */
 public class Clyde extends Ghost {
     
     /**
-     * 
-     * @param node
+     * TODO: definir a velocidade do fantasma
+     * Construtor padrão da classe Clyde
+     * @param node Nó em que o fantasma nasce
      */
     public Clyde (Node node) {
         
-        // definir velocidade
         super(node, GameConstants.CLYDE, 0, GameConstants.LEFT);
         
     }
     
     /**
-     * 
+     * Método que movimenta o fantasma de acordo do modo em que o
+     * fantasma está, ou seja, se ele está saindo da casa, andando
+     * aleatoriamente no tabuleiro ou voltando para casa.
      */
     public void move() {
 
