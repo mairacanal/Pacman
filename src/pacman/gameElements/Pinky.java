b29310179b832ce5645d3b62c529157221bdc5ae
Maíra <maira.canal@usp.br>
2021-11-18 16:01:17 -0300

    Pinky: Add Javadoc

--- src/pacman/gameElements/Pinky.java
+++ src/pacman/gameElements/Pinky.java
@@ -1,23 +1,25 @@
 package pacman.gameElements;
 
 /**
- * 
+ * Classe que representa um elemento do jogo, o fantasma Pinky 
  */
 public class Pinky extends Ghost {
     
     /**
-     * 
-     * @param node
+     * TODO: definir a velocidade do fantasma
+     * Construtor padrão da classe Pinky
+     * @param node Nó em que o fantasma nasce
      */
     public Pinky(Node node) {
         
-        // definir velocidade
         super(node, GameConstants.PINKY, 0, GameConstants.LEFT);
         
     }    
     
     /**
-     * 
+     * Método que movimenta o fantasma de acordo do modo em que o
+     * fantasma está, ou seja, se ele está saindo da casa, perseguindo
+     * o Pacman ou voltando para casa.
      */
     public void move() {
 
