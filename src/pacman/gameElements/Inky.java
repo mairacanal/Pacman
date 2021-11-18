66e844cba3882a3ab1d43795b8d779d49ea8291d
Maíra <maira.canal@usp.br>
2021-11-18 16:00:56 -0300

    Inky: Add Javadoc

--- src/pacman/gameElements/Inky.java
+++ src/pacman/gameElements/Inky.java
@@ -1,23 +1,25 @@
 package pacman.gameElements;
 
 /**
- * 
+ * Classe que representa um elemento do jogo, o fantasma Inky 
  */
 public class Inky extends Ghost {
 
     /**
-     * 
-     * @param node
+     * TODO: definir a velocidade do fantasma
+     * Construtor padrão da classe Inky
+     * @param node Nó em que o fantasma nasce
      */
     public Inky(Node node) {
         
-        // definir identificaçao e velocidade
         super(node, GameConstants.INKY, 0, GameConstants.RIGHT);
         
     }
     
     /**
-     * 
+     * Método que movimenta o fantasma de acordo do modo em que o
+     * fantasma está, ou seja, se ele está saindo da casa, andando
+     * aleatoriamente no tabuleiro ou voltando para casa.
      */
     public void move() {
 
